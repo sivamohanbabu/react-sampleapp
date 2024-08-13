9b8d3dc7e2f5b146b0e08a351bf79efc02741c6f
siva veludurthi <78125303+sivamohanbabu@users.noreply.github.com>
2024-08-13 11:02:52 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hi Siva Welcome to React App
         </a>
       </header>
     </div>
